a961d0330dcbde1a42d148d279901024481f1fa3
innovation <zzkkjj@foxmail.com>
2017-12-26 18:12:27 +0800

    fix some bug

--- app/models.py
+++ app/models.py
@@ -2,7 +2,23 @@
 from datetime import datetime
 import sys
 sys.path.append("..")
-from app import db
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import pymysql, os
+from app.home import  home as home_blueprint
+from app.admin import admin as admin_blueprint
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:123@127.0.0.1/movie"
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = True
+app.config["SECRET_KEY"] = "ZHANGSan"
+app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/upload/')
+app.debug = True
+
+app.register_blueprint(home_blueprint)
+app.register_blueprint(admin_blueprint, url_prefix="/admin")
+db = SQLAlchemy(app)
+
 
 
 
